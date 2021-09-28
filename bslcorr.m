e9beb607df2bbce78b1b15b5ae1b6b47724e6e52
akeilCSEA <akeil@ufl.edu>
2021-09-27 22:04:36 -0400

    initial commit
    initial commit

---
+++ bslcorr.m
@@ -0,0 +1,1 @@
+% bslcorrmat% corrects 129 channel ar mat % by subtracting mean of baseline in samplepointsfunction [data] = bslcorr(inmat, bslvec);if isempty (bslvec),     bslvec = 1:size(inmat,2); endfor chan = 1 : size(inmat, 1)data(chan,:) = inmat(chan,:)-mean(inmat(chan,bslvec),2);end
