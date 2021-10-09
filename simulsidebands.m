3978d788507c3c548ec94db11bd346886c60f8bb
akeilCSEA <52578563+akeilCSEA@users.noreply.github.com>
2021-10-09 10:15:46 -0400

    add a quant measure of leaking

--- simulsidebands.m
+++ simulsidebands.m
@@ -20,9 +20,9 @@ freq2 = 5;
 % simulated trial). This will be used to complete the "source SNR" origSNR
 % in the simulated data, before windowing
 
-noiselevel = 20;
+noiselevel = 40;
 
-for run = 1:50
+for run = 1:100
   for siglevel1 = 1:20
     for siglevel2 = 1:15
 
@@ -56,6 +56,7 @@ end
 origSNR1 = ((1:20)./3)./noiselevel; % an estimate of the original SNR in the input signal
 origSNR2 = ((1:15)./2)./noiselevel;
 
+% plot the output amplitude estimate
 figure
 subplot(1,2,1), plot(origSNR2, squeeze(mean(trialmagf1,3))), 
 l1 = legend(num2str(origSNR1')); title(l1, 'Input SNR at Tag 1')
@@ -67,17 +68,25 @@ l1 = legend(num2str(origSNR2')); title(l1, 'Input SNR at Tag 2')
 xlabel(['Input SNR at tag 1 (' num2str(freq1) 'Hz)']), ylabel(['Amplitude for tag 2 (' num2str(freq2) 'Hz)'])
 title('Tag 2 amplitude by Tag 1 input SNR') 
 
+% now plot the output SNR
 figure
 subplot(1,2,1), plot(origSNR2, squeeze(mean(trialSNRf1,3))), legend(num2str(origSNR1'))
 l1 = legend(num2str(origSNR1')); title(l1, 'Input SNR at Tag 1')
 xlabel(['Input SNR at tag 2 (' num2str(freq2) 'Hz)']), ylabel(['Output SNR for tag 1 (' num2str(freq1) 'Hz)']),
 title('Tag 1 output SNR by Tag 2 input SNR') 
 
-
 subplot(1,2,2), plot(origSNR1, squeeze(mean(trialSNRf2,3))), 
 l1 = legend(num2str(origSNR2')); title(l1, 'Input SNR at Tag 2')
 xlabel([' Input SNR at tag 1 (' num2str(freq1) 'Hz)']), ylabel(['Output SNR for tag 2 (' num2str(freq2) 'Hz)'])
 title('Tag 2 output SNR by Tag 1 input SNR') 
 
+% percent effect of one tag on the other, measured as percentage change 
+% between low competing tag SNR and high competing tag SNR, relative to
+% mean amplitude across tag 2 sigbal levels 
+amp_percentTag1 = 100.*(mean(mean(trialmagf1(:, 1:5, :),3),2) - mean(mean(trialmagf1(:, end-4:end  , :),3),2))./mean(mean(trialmagf1(:, :, :),3), 2);
+amp_percenTag2 = 100.*(mean(mean(trialmagf2(1:5, :, :),3),1) - mean(mean(trialmagf2(end-4:end, :  , :),3),1))./mean(mean(trialmagf2(:, :, :),3), 1);
 
+% the same measure for mean output SNR
+SNR_percentTag1 = 100.*(mean(mean(trialSNRf1(:, 1:5, :),3),2) - mean(mean(trialSNRf1(:, end-4:end  , :),3),2))./mean(mean(trialSNRf1(:, :, :),3), 2);
+SNR_percenTag2 = 100.*(mean(mean(trialSNRf2(1:5, :, :),3),1) - mean(mean(trialSNRf2(end-4:end, :  , :),3),1))./mean(mean(trialSNRf2(:, :, :),3), 1);
 
